b8737ff4bf82a8b6a1724c77f7d86c4ab119678e
nkastanas <nkastanas@incarpc.gr>
2015-10-08 23:28:23 +0300

    new

---
+++ readpath.ps1
@@ -0,0 +1,12 @@
+﻿# Show an Open Folder Dialog and return the directory selected by the user.
+function FolderBrowserDialog([string]$Message, [string]$InitialDirectory)
+{
+    $app = New-Object -ComObject Shell.Application
+    $folder = $app.BrowseForFolder(0, $Message, 0, $InitialDirectory)
+    if ($folder) { return $folder.Self.Path } else { return '' }
+}
+
+$sourcepath = FolderBrowserDialog
+write-host "value: $sourcepath"
+get-vm
+$hostname = Read-Host -Prompt "VM Name"
